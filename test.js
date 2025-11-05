362c5cd7abba68763b4ae656b16ba612e8a312a3
Iulian <iulianboerescu16@gmail.com>
2025-11-05 19:34:53 +0200

    tested variables

---
+++ test.js
@@ -0,0 +1,40 @@
+// var x = 5;
+// let y = 6;
+// const z =7;
+
+// x = 6;
+// console.log(x);
+// y = 7;
+// console.log(y);
+// z = 8;
+// console.log(z);
+
+// console.log(x,y,z);
+
+
+// function testVariables(param){
+//     if(param === true) {
+//         var x = 5;
+//         let y = 6;
+//         const z = 7; 
+//     }
+//     console.log(x);
+//     console.log(y);
+//     console.log(z);
+// }
+
+// testVariables(true);
+
+ const testVariables2 = (param) =>{
+    console.log(x);
+     if(param === true) {
+        var x = 5;
+        let y = 6;
+        const z = 7; 
+    }
+    console.log(x);
+    console.log(y);
+    console.log(z);
+ }
+
+ testVariables2(true);
